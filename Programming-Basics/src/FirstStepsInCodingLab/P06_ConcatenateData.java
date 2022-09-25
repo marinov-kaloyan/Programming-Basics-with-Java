6d8c78b551711ee0d74e902a9bb8266d2a260aeb
Arraxiel <kaloyanmarinov93@gmail.com>
2022-09-26 00:10:09 +0300

    06. Concatenate Data

---
+++ Programming-Basics/src/FirstStepsInCodingLab/P06_ConcatenateData.java
@@ -0,0 +1,14 @@
+package FirstStepsInCodingLab;
+
+import java.util.Scanner;
+
+public class P06_ConcatenateData {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String FirstName = scanner.nextLine();
+        String SecondName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine()  );
+        String town =scanner.nextLine();
+        System.out.printf("You are %s %s, a %d-years old person from %s.",FirstName, SecondName,age ,town);
+    }
+}
