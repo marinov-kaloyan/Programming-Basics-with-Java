b7f745bf7ae5c1e538a5ecfb3979081c76fb27e0
Arraxiel <kaloyanmarinov93@gmail.com>
2022-09-26 00:08:20 +0300

    05. Greeting By Name

---
+++ Programming-Basics/src/FirstStepsInCodingLab/P05_GreetingByName.java
@@ -0,0 +1,12 @@
+package FirstStepsInCodingLab;
+
+import java.util.Scanner;
+
+public class P05_GreetingByName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, "+ name + "!");
+
+    }
+}
