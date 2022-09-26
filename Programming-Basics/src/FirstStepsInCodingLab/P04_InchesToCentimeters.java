aa3866493a457808bf609451a58a076a0b01c468
Arraxiel <kaloyanmarinov93@gmail.com>
2022-09-26 16:38:01 +0300

    04. Inches to Centimeters

--- Programming-Basics/src/FirstStepsInCodingLab/P04_InchesToCentimeters.java
+++ Programming-Basics/src/FirstStepsInCodingLab/P04_InchesToCentimeters.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class P04_InchesToCentimeters {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+        Scanner  scanner = new Scanner (System.in);
         double a = Double.parseDouble(scanner.nextLine());
         double cmToInch = a * 2.54;
         System.out.println(cmToInch);
