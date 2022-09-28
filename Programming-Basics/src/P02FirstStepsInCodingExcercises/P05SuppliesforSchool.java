d7f2bc9cd3739aab9be18aab7bf656de8956b48a
Arraxiel <kaloyanmarinov93@gmail.com>
2022-09-28 23:32:58 +0300

    05. Supplies for School

---
+++ Programming-Basics/src/P02FirstStepsInCodingExcercises/P05SuppliesforSchool.java
@@ -0,0 +1,17 @@
+package P02FirstStepsInCodingExcercises;
+
+import java.util.Scanner;
+
+public class P05SuppliesforSchool {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int himikali = Integer.parseInt(scanner.nextLine());
+        int markeri = Integer.parseInt(scanner.nextLine());
+        int litri = Integer.parseInt(scanner.nextLine());
+        double procent = Double.parseDouble(scanner.nextLine());
+
+
+        double vsichko = (1 - procent / 100) * (himikali * 5.80 + markeri * 7.20 + litri * 1.20);
+        System.out.println(vsichko);
+    }
+}
