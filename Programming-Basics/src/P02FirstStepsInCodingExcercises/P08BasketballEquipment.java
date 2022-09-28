41883833113ee7c98be798c6de3b4053e1e373a1
Arraxiel <kaloyanmarinov93@gmail.com>
2022-09-28 23:35:58 +0300

    08. Basketball Equipment

---
+++ Programming-Basics/src/P02FirstStepsInCodingExcercises/P08BasketballEquipment.java
@@ -0,0 +1,16 @@
+package P02FirstStepsInCodingExcercises;
+
+import java.util.Scanner;
+
+public class P08BasketballEquipment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double   taksa =Double.parseDouble(scanner.nextLine());
+        double kecove = 0.6*taksa;
+        double ekip = 0.8*kecove;
+        double topka = 0.25* ekip;
+        double aksesoari = topka*0.2;
+        double suma = taksa + kecove + ekip + topka + aksesoari;
+        System.out.println(suma);
+    }
+}
