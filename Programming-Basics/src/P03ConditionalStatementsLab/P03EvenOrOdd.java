7b86e337f45e97f30f3df662d7584ad839cac3dd
Arraxiel <kaloyanmarinov93@gmail.com>
2022-09-30 23:45:17 +0300

    03. Even or Odd

---
+++ Programming-Basics/src/P03ConditionalStatementsLab/P03EvenOrOdd.java
@@ -0,0 +1,12 @@
+package P03ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class P03EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        if (a%2==0){System.out.println("even");}
+        else{System.out.println("odd");}
+    }
+}
