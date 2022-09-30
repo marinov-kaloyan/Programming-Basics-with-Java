75037f3f9d42b85cae1a15b46ab10ee20be3d3e7
Arraxiel <kaloyanmarinov93@gmail.com>
2022-09-30 23:46:51 +0300

    05. Number 100...200

---
+++ Programming-Basics/src/P03ConditionalStatementsLab/P05Number100_200.java
@@ -0,0 +1,13 @@
+package P03ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class P05Number100_200 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        if (a<100){System.out.println("Less than 100");}
+        else if (a<=200){System.out.println("Between 100 and 200");}
+        else {System.out.println("Greater than 200");}
+    }
+}
