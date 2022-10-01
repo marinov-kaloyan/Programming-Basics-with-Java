d2ed27ed5d3322354222f6e6590a83863f3b601e
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-01 19:19:06 +0300

    01. Sum Seconds

---
+++ Programming-Basics/src/P04ConditionalStatementsExercises/P01SumSeconds.java
@@ -0,0 +1,19 @@
+package P04ConditionalStatementsExercises;
+
+import java.util.Scanner;
+
+public class P01SumSeconds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int timeFirst = Integer.parseInt(scanner.nextLine());
+        int timeSecond = Integer.parseInt(scanner.nextLine());
+        int timeThird = Integer.parseInt(scanner.nextLine());
+        int totalTime = timeFirst+timeSecond+timeThird;
+        int minutes = totalTime/60;
+        int seconds = totalTime%60;
+        if(totalTime<10){System.out.printf("0:0%d",seconds);}
+        else if (seconds<10){System.out.printf("%d:0%d",minutes,seconds);}
+        else {System.out.printf("%d:%d",minutes,seconds);}
+
+    }
+}
