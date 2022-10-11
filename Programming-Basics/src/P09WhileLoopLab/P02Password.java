8b68bbbb2a20d7f5db76bf54a14ee1df12d14797
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-11 23:26:59 +0300

    02. Password

---
+++ Programming-Basics/src/P09WhileLoopLab/P02Password.java
@@ -0,0 +1,16 @@
+package P09WhileLoopLab;
+
+import java.util.Scanner;
+
+public class P02Password {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        String password = scanner.nextLine();
+        String input = scanner.nextLine();
+        while(!input.equals(password)){
+            input= scanner.nextLine();
+        }
+        System.out.println("Welcome "+username+'!');
+    }
+}
