0420fdce22bf247bda6819423a3f5a9fb7bf3e56
Arraxiel <kaloyanmarinov93@gmail.com>
2022-10-21 23:16:01 +0300

    02. Equal Sums Even Odd Position

--- Programming-Basics/src/P12NestedLoopsExercise/P02EqualSumsEvenOddPosition.java
+++ Programming-Basics/src/P12NestedLoopsExercise/P02EqualSumsEvenOddPosition.java
@@ -9,22 +9,23 @@ public class P02EqualSumsEvenOddPosition {
         int last = Integer.parseInt(scanner.nextLine());
 
 
-        for (int j = first;j  <=last ; j++){
-            int currentNum=j;
+        for (int j = first; j <= last; j++) {
+            int currentNum = j;
             int sumeven = 0;
             int sumodd = 0;
-            for(int i =6;i>=0;i--){
-                int digit = currentNum%10;
-                if(i%2==0){sumeven=sumeven+digit;}
-                else {sumodd=sumodd+digit;}
-                currentNum=currentNum/10;
+            for (int i = 6; i >= 0; i--) {
+                int digit = currentNum % 10;
+                if (i % 2 == 0) {
+                    sumeven = sumeven + digit;
+                } else {
+                    sumodd = sumodd + digit;
+                }
+                currentNum = currentNum / 10;
 
 
             }
-            if(sumeven==sumodd)System.out.print(j+" ");
+            if (sumeven == sumodd) System.out.print(j + " ");
         }
-
-
 
 
     }
